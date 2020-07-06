5e024595048e5305915d990dbbf7df080a87ecd5
Anderson Rodriguez Molina <anderson_rodriguez@ucne.edu.do>
2020-07-06 14:28:34 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2doParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+    }
+}
